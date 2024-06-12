4d55f5364921a57ce1b09d9e5e6e00b176560ba5
siperzen <156843337+siperzen@users.noreply.github.com>
2024-06-12 22:49:41 +0500

    add operate function, link javascript to DOM digits and the result display

--- script.js
+++ script.js
@@ -18,3 +18,20 @@ let firstNumber;
 let calculationOperator;
 let secondNumber;
 
+function operate(calculationOperator, firstNumber, secondNumber) {
+    add(firstNumber, secondNumber);
+}
+
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+const three = document.querySelector(".three");
+const four = document.querySelector(".four");
+const five = document.querySelector(".five");
+const six = document.querySelector(".six");
+const seven = document.querySelector(".seven");
+const eight = document.querySelector(".eight");
+const nine = document.querySelector(".nine");
+//  make the resultdisplay show the clicked buttons
+const resultDisplay = document.querySelector(".result-display");
+
+
